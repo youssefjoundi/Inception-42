0f3e6e681389c1d5695d10acf3fd668031837a6f
Youssef Joundi <yojoundi@student.1337.ma>
2023-06-13 00:24:27 +0100

    Add: Mariadb script Done

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -3,21 +3,26 @@
 echo "## Start Mysql service....."
 service mysql start
 
+# Wait for the MySQL service to start
+sleep 1
+
+# Set password to the root
 mysql -e "FLUSH PRIVILEGES; ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
 
 echo "## Create Wordpress Database ....."
-mysql -e "CREATE DATABASE wordpress;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE wordpress;"
 
 echo "## Create User for wordpress database..."
-mysql -e "CREATE USER 'wordpressuser'@'%' IDENTIFIED BY 'wordpresspass';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER 'wordpressuser'@'%' IDENTIFIED BY 'wordpresspass';"
 
 echo "## Give user privileges to database ..."
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'%';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'%';"
+
+#Shut dowm mysql
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
 echo "## Run mysql server in foreground"
-
-#Shutdown script
-#service mysql stop
+mysqld
 
 
-#mysqld
+
